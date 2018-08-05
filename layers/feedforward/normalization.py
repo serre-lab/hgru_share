be1bb70ef45508f7ea94eec3e786525b769618e1
Drew Linsley <drewlinsley@gmail.com>
2018-08-04 21:42:57 -0400

    Update normalization.py

--- layers/feedforward/normalization.py
+++ layers/feedforward/normalization.py
@@ -6,11 +6,13 @@ def batch(
         name,
         scale=True,
         center=True,
+        renorm=True,
         training=True):
     return tf.layers.batch_normalization(
         inputs=bottom,
         name=name,
         scale=scale,
         center=center,
+        renorm=renorm,
         training=training)
 
